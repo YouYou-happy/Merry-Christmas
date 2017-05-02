9b07fdca69acf292aed3b04a877f748146d72d6e
YouYou-happy <18251825510@163.com>
2017-05-02 18:51:25 +0800

    BGM
    场景音乐

--- christmas.js
+++ christmas.js
@@ -4,69 +4,37 @@
  * @type {Object}
  */
 
+
 /**
- * 切换页面
- * 模拟镜头效果
- * @return {[type]} [description]
+ * 背景音乐
+ * @param {[type]} url  [description]
+ * @param {[type]} loop [description]
  */
-function changePage(element, effect, callback) {
-    element
-        .addClass(effect)
-        .one("animationend webkitAnimationEnd", function() {
-            callback && callback();
-        })
+function HTML5Audio(url, loop) {
+    var audio = new Audio(url);
+    audio.autoplay = true;
+    audio.loop = loop || false; //是否循环
+    audio.play();
+    return {
+        end: function(callback) {
+            audio.addEventListener('ended', function() {
+                callback()
+            }, false);
+        }
+    }
 }
 
 
-/**
- * 中间调用
- */
-var Christmas = function() {
-    //页面容器元素
-    var $pageA = $(".page-a");
-    var $pageB = $(".page-b");
-    var $pageC = $(".page-c");
-
-    //观察者
-    var observer = new Observer();
-
-    //A场景页面
-    new pageA(function() {
-        observer.publish("completeA");
-    })
-    //进入B场景
-    observer.subscribe("pageB", function() {
-        new pageB(function() {
-            observer.publish("completeB");
-        })
-    })
-    //进入C场景
-    observer.subscribe("pageC", function() {
-        new pageC()
-    })
-
-
-    //页面A-B场景切换
-    observer.subscribe("completeA", function() {
-        changePage($pageA, "effect-out", function() {
-            observer.publish("pageB");
-        })
-    })
-    //页面B-C场景切换
-    observer.subscribe("completeB", function() {
-        changePage($pageC, "effect-in", function() {
-            observer.publish("pageC");
-        })
-    })
-
-};
-
-
-
-
 $(function() {
-    $("button").click(function(){
-        //圣诞主题效果，开始
-        Christmas()
+    $("button:first").click(function() {
+        //背景音乐
+        var audio1 = HTML5Audio('http://www.imooc.com/upload/media/one.mp3')
+        audio1.end(function() {
+            alert("音乐结束")
+        })
+    })
+    $("button:last").click(function() {
+        //循环播放那
+        Hmlt5Audio('http://www.imooc.com/upload/media/two.mp3', true)// ? url = http://www.imooc.com/upload/media/two.mp3'
     })
 })
